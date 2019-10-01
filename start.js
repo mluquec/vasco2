4ca266997a1f6030be726bb8880d5dcc28ec0c7a
mluquec <31400104+mluquec@users.noreply.github.com>
2019-10-01 13:34:40 +0200

    Update start.js

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-const server = app.listen(8080, () => {
+const server = app.listen(3000, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
